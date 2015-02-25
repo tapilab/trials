394ab4e244f35173353716aae6f7f4fb0f5f7ec8
Aron Culotta <aronwc@gmail.com>
2015-02-25 14:26:58 -0600

    add example form

--- searchapp/app/views.py
+++ searchapp/app/views.py
@@ -1,13 +1,19 @@
 from app import app
+from flask import render_template
+from flask_wtf import Form
+from wtforms import StringField
+from wtforms.validators import DataRequired
+
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello, World!"
 
+
 @app.route('/search')
 def search():
-    user = { 'nickname': 'Miguel' } # fake user
+    user = { 'nickname': 'Miguel' }  # fake user
     return '''
 <html>
   <head>
@@ -18,3 +24,18 @@ def search():
   </body>
 </html>
 '''
+
+
+@app.route('/testform/', methods=['GET', 'POST'])
+def testform():
+    form = MyForm()
+    if form.validate_on_submit():
+        return '<html>your name is %s</html>' % form.name.data
+    return render_template('form.html', form=form)
+
+
+class MyForm(Form):
+    name = StringField('name', validators=[DataRequired()])
+
+    def __init__(self):
+        super(MyForm, self).__init__(csrf_enabled=False)
